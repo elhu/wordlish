c69e42d7c55b8636fc2b9717144e69bc48df0971
Vincent Boisard <boisard.v@gmail.com>
2022-01-14 19:42:06 +0100

    Basic Game display with Hotwire

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,12 @@
 class GamesController < ApplicationController
+  before_action :set_game, except: [:index]
+
+  def show
+
+  end
+
+  private
+  def set_game
+    @game = Game.find_by!(uuid: params[:id])
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 require 'uuid'
 
 class Game < ApplicationRecord
+  broadcasts
+
   enum status: { ongoing: "ongoing", done: "done" }, _prefix: true
 
   validates :uuid, presence: true, uniqueness: true
