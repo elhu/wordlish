eb3b4ab321fc5046973b1970d30cff830dbb2930
Vincent Boisard <boisard.v@gmail.com>
2022-01-20 08:24:30 +0100

    Colorize the results of each guess

--- app/helpers/attempts_helper.rb
+++ app/helpers/attempts_helper.rb
@@ -1,2 +1,11 @@
 module AttemptsHelper
+  def letter_class(word, letter, idx)
+    if letter == word.to_guess[idx]
+      'dark:bg-green-500'
+    elsif word.to_guess.include?(letter)
+      'bg-yellow-300'
+    else
+      'dark:bg-gray-800'
+    end
+  end
 end
